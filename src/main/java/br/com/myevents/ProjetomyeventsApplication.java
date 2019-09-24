5ee571ec03c2b1c9eaaadccb75a1c07a1115dc49
gust44444 <gustavoheidemann1@gmail.com>
2019-09-24 15:50:31 -0300

    inital commit

---
+++ src/main/java/br/com/myevents/ProjetomyeventsApplication.java
@@ -0,0 +1,13 @@
+package br.com.myevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomyeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomyeventsApplication.class, args);
+	}
+
+}
