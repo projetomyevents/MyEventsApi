6b6718e71a867285dfda5c04a58e0c67b0092fe6
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-10-08 16:20:08 -0300

    Implementar validações e documentar classe 'Evento'

--- src/main/java/br/com/myevents/domain/Evento.java
+++ src/main/java/br/com/myevents/domain/Evento.java
@@ -1,21 +1,33 @@
 package br.com.myevents.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Positive;
-
-import lombok.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 
 /**
  * Representa um evento.
  */
 @Entity
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -31,18 +43,60 @@ public class Evento implements Serializable {
      */
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Setter(value = AccessLevel.NONE) private long id;
+    private Long id;
 
-    // TODO: arrumar essa classe
-    private String nomeCompleto;
-    private LocalDate data;
-    private String precoEntrada;
-    private byte limiteAcompanhantes;
-    private byte idadeMinima;
+    /**
+     * O nome do evento.
+     */
+    @Column(nullable = false)
+    @NotBlank private String nome;
+
+    /**
+     * O data de inicio do evento.
+     */
+    @Column(nullable = false)
+    @NotNull @Future private LocalDate data;
+
+    /**
+     * O preço de entrada do evento.
+     */
+    private String preco;
+
+    /**
+     * O limite de acompanhantes padrão de cada convidado.
+     */
+    @Column(nullable = false)
+    @NotNull @PositiveOrZero private byte limiteAcompanhantes;
+
+    /**
+     * A idade mínima permitida no evento.
+     */
+    @PositiveOrZero private byte idadeMinima;
+
+    /**
+     * O traje recomendado para o evento.
+     */
     private String traje;
+
+    /**
+     * A descrição do evento.
+     */
     private String descricao;
+
+    /**
+     * A imagem ilustrativa do evento.
+     */
     private byte[] imagem;
-    private String cronograma;
-    private byte[] anexos;
+
+    /**
+     * O cronograma do evento.
+     */
+    @Column(nullable = false)
+    @NotBlank private String cronograma;
+
+    /**
+     * Os arquivos em anexo do evento.
+     */
+    private List<byte[]> anexos;
 
 }
