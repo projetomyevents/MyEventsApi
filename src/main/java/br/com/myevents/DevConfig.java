9b92d261dd451dc62331355480de2632ff62daa6
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-10-28 16:00:02 -0300

    Adicionar usuário administrador quando o app iniciar em modo teste

--- src/main/java/br/com/myevents/DevConfig.java
+++ src/main/java/br/com/myevents/DevConfig.java
@@ -1,10 +1,14 @@
 package br.com.myevents;
 
+import br.com.myevents.model.User;
+import br.com.myevents.repository.UserRepository;
+import br.com.myevents.security.enums.Role;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Configuração de uma execução do aplicativo em modo dev.
@@ -15,10 +19,23 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class DevConfig {
 
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
     /**
      * Preencher a base de dados com dados de amostra para testes.
      */
     @Bean
-    protected void populateDatabaseForTesting() { }
+    protected void populateDatabaseForTesting() {
+        User adminUser = User.builder()
+                .email("admin@admin")
+                .password(passwordEncoder.encode("123456"))
+                .name("admin")
+                .CPF("52829612884")
+                .phone("8888888888")
+                .role(Role.ADMIN.getId())
+                .build();
+        userRepository.save(adminUser);
+    }
 
 }
