81f307074939a5817105343c48dcd0acee7054a2
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-11-04 15:13:42 -0200

    Adicionar docs e anotações de compilação ao que já foi feito

--- src/main/java/br/com/myevents/model/City.java
+++ src/main/java/br/com/myevents/model/City.java
@@ -1,15 +1,44 @@
 package br.com.myevents.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+/**
+ * Representa uma cidade.
+ */
 @Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString
 public class City implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
+    /**
+     * A chave primária da cidade.
+     */
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
+
+    /**
+     * O nome da cidade.
+     */
     private String name;
 }
