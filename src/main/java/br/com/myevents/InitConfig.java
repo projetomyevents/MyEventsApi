8fc4714a252135ea708b359de42af90f1c2123ec
Ademir Ferreira <ademir.ferreira.zorah@gmail.com>
2019-11-05 14:03:59 -0200

    Implementar adição de Estados e Cidades do Brasil a base de dados

--- src/main/java/br/com/myevents/InitConfig.java
+++ src/main/java/br/com/myevents/InitConfig.java
@@ -1,8 +1,17 @@
 package br.com.myevents;
 
+import br.com.myevents.model.City;
+import br.com.myevents.model.State;
+import br.com.myevents.model.dto.CityDTO;
+import br.com.myevents.model.dto.StateDTO;
+import br.com.myevents.repository.CityRepository;
+import br.com.myevents.repository.StateRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Configuração de uma execução do aplicativo.
@@ -10,4 +19,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class InitConfig {
+
+    private final CityRepository cityRepository;
+    private final StateRepository stateRepository;
+
+    /**
+     * Registrar todos os estados do Brasil na base de dados.
+     */
+    @Bean
+    protected void initializeStates() throws Exception {
+        for (StateDTO state :new ObjectMapper().readValue(
+                new ClassPathResource("br_states.json").getInputStream(), StateDTO[].class)) {
+            stateRepository.save(State.builder()
+                    .id(state.getId())
+                    .name(state.getName())
+                    .build());
+        }
+    }
+
+    /**
+     * Registrar todos as cidades do Brasil na base de dados.
+     */
+    @Bean
+    protected void initializeCities() throws Exception {
+        for (CityDTO city :new ObjectMapper().readValue(
+                new ClassPathResource("br_cities.json").getInputStream(), CityDTO[].class)) {
+            cityRepository.save(City.builder()
+                    .id(city.getId())
+                    .name(city.getName())
+                    .state(State.builder().id(city.getStateId()).build())  // só precisamos do id do estado
+                    .build());
+        }
+    }
+
 }
